63d3b580ea02e0844555d7a78efd641b3fb74577
Paul Traue <ptrauejr@gmail.com>
2019-06-14 17:23:49 -0500

    Styled show page and added links to show and edit views

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190614212950) do
+ActiveRecord::Schema.define(version: 20190614220953) do
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale",        limit: 65535
-    t.datetime "created_at",                                                null: false
-    t.datetime "updated_at",                                                null: false
+    t.datetime "created_at",                                                            null: false
+    t.datetime "updated_at",                                                            null: false
     t.integer  "user_id",          limit: 4
-    t.integer  "status",           limit: 4,                    default: 0
-    t.decimal  "overtime_request",               precision: 10, default: 0
+    t.integer  "status",           limit: 4,                              default: 0
+    t.decimal  "overtime_request",               precision: 10, scale: 2, default: 0.0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
