7979df197c395737d8537614aade93739e4d87aa
Paul Traue <ptrauejr@gmail.com>
2019-06-18 22:59:27 -0500

    Adds phone attribute to User and made it required

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -22,6 +22,7 @@ class UserDashboard < Administrate::BaseDashboard
       created_at: Field::DateTime.with_options(searchable: false),
       updated_at: Field::DateTime.with_options(searchable: false),
       type: Field::String.with_options(searchable: false),
+      phone: Field::String.with_options(searchable: false),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -41,6 +42,7 @@ class UserDashboard < Administrate::BaseDashboard
       :posts,
       :id,
       :email,
+      :phone,
       :sign_in_count,
       :current_sign_in_at,
       :last_sign_in_at,
@@ -61,6 +63,7 @@ class UserDashboard < Administrate::BaseDashboard
       :password,
       :first_name,
       :last_name,
+      :phone,
   ].freeze
 
   # Overwrite this method to customize how admin users are displayed
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190614220953) do
+ActiveRecord::Schema.define(version: 20190619034505) do
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20190614220953) do
     t.string   "type",                   limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "phone",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
